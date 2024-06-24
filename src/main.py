dfa5b1754eec005a280d97de2063269222fd7042
gerlanno <gerlanno@gmail.com>
2024-06-24 14:34:39 -0300

    commit 1a fase

---
+++ src/main.py
@@ -0,0 +1,43 @@
+import sys
+import os
+import pprint
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+from config import settings
+from serpapi import GoogleSearch
+from tasks import processar_dados
+from model import inserir_dados, inicializar_bd
+
+
+def google_maps(query):
+    # Criar a tabela, caso nao exista, de acordo com a classe mapeada.
+    inicializar_bd()
+
+    query = query
+
+    params = {
+        "engine": "google_maps",
+        "q": query,
+        "type": "search",
+        "hl": "pt-br",
+        "api_key": settings.API_KEY,
+    }
+
+    search = GoogleSearch(params)
+    results = search.get_dict()
+
+    # Tratando os dados antes de inseri-los no banco.
+    inserir_dados((processar_dados(results)))
+
+
+try:
+    operacao = sys.argv[1].lower()
+
+    if operacao == "maps":
+
+        query = sys.argv[2]
+        google_maps(query)
+    else:
+        print("Erro!")
+except Exception as e:
+    print(f"erro: {e}")
---
+++ src/model.py
@@ -0,0 +1,40 @@
+from ast import Set
+import sqlalchemy
+from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy import Column, Integer, String
+
+# Criando a engine do SQLAlchemy que se conecta a um banco de dados SQLite chamado "results.db"
+engine = sqlalchemy.create_engine("sqlite:///results.db")
+# Criando uma classe base para a definição das classes de mapeamento ORM
+Base = declarative_base()
+# Criando uma classe Session configurada para ser ligada à engine
+Session = sessionmaker(bind=engine)
+# Criando uma instância de sessão para interagir com o banco de dados
+session = Session()
+
+# Classe mapeada que representa uma tabela no banco de dados
+class Result(Base):
+    # Nome da tabela no banco de dados.
+    __tablename__ = "results"
+    # Colunas da tabela com seus respectivos tipos e propriedades.
+    id = Column(Integer, primary_key=True)
+    nome = Column(String(100))
+    logradouro = Column(String(100))
+    bairro = Column(String(100))
+    cidade = Column(String(50))
+    estado = Column(String(20))
+    cep = Column(String(20))
+    telefone = Column(String(20))
+    website = Column(String(150))
+
+    def __repr__(self):
+        return f"<Endereco(id={self.id}, nome='{self.nome}', logradouro='{self.logradouro}', bairro='{self.bairro}', cidade='{self.cidade}', estado='{self.estado}', cep='{self.cep}', telefone='{self.telefone}', website='{self.website}')>"
+
+def inserir_dados(lista):
+    # Inserir a lista com dados no banco de dados.
+    session.add_all(lista)
+    session.commit()
+
+
+def inicializar_bd():
+    Base.metadata.create_all(engine)
